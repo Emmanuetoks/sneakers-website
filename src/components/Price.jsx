296fdb1ff52c826d855ee23a59af60f43d45d729
ArmyofDeux <103246856+Emmanuetoks@users.noreply.github.com>
2023-05-23 06:18:09 -0700

    Add fontface

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -5,16 +5,16 @@ import countContext from "../context/itemCount";
 const Price = () => {
   const [price, setPrice] = useState({ mainPrice: 250, discount: 50 });
   return (
-    <div className="price-section">
+    <div className="price-section flex">
       <h3 className="text-accent-400 fw-300">
-        {`$ ${
+        {`$${
           price.discount === 0
             ? price.mainPrice
             : (price.mainPrice * price.discount) / 100
-        }`}{" "}
-        <span className="text-accent-500 bg-light-orange fw-200">{price.discount}%</span>
+        }`}{".00 "}
+        <span className="text-accent-500 discount bg-light-orange fw-200">{price.discount}%</span>
       </h3>
-      <h5 className="text-accent-200 fw-200">{`$ ${price.discount != 0 ? price.mainPrice : ""}`}</h5>
+      <h5 className="text-accent-200 fw-200">{`$${price.discount != 0 ? price.mainPrice : ""}.00`}</h5>
     </div>
   );
 };
--- src/components/UserInfoSection.jsx
+++ src/components/UserInfoSection.jsx
@@ -6,7 +6,8 @@ import countContext from "../context/itemCount";
 import DeleteImg from "../../public/images/icon-delete.svg";
 import { ItemContext } from "../context/itemContext";
 
-const CartItems = ({ src, price, amount, name, setCartItems }) => {
+const CartItem = ({ src, price, amount, name, setCartItems }) => {
+  const [cartCount, setCartCount] = useContext(countContext);
   const deleteItem = (e) => {
     const targetItem = e.target.getAttribute("target-item");
     setCartItems((prevValue) => {
@@ -16,15 +17,18 @@ const CartItems = ({ src, price, amount, name, setCartItems }) => {
         }
       });
     });
+
+    setCartCount(0)
+
   };
   return (
     <div className="cart__items" style={{'marginBlock': '1rem'}} >
       <figure className="flex" style={{ alignItems: "center"}} >
         <img style={{ width: "2rem" }} src={src} />
         <figcaption>
-            <p className="text-accent-300">{name}</p>
-            <span className="text-accent-300">{`${price} x ${amount} `}</span>
-            <span className="text-accent-400 fw-300">{amount * price}</span>
+            <p className="text-accent-300 capitalize">{name}</p>
+            <span className="text-accent-300">{`$${price}.00 x ${amount}  `}</span>
+            <span className="text-accent-400 fw-300">{'$' + amount * price}</span>
         </figcaption>
         <button
           onClick={deleteItem}
@@ -45,7 +49,7 @@ const UserCartInfo = () => {
       <header className="cart__header text-accent-400 fw-200">Cart</header>
       {cartItems.length != 0 ?cartItems.map((item) => {
         return (
-          <CartItems
+          <CartItem
             key={item.name}
             name={item.name}
             src={item.src}
@@ -57,7 +61,7 @@ const UserCartInfo = () => {
       }) : <p className="text-accent-200 fw-200" style={{'textAlign':'center', 'marginBottom': '1em'}}>Your cart is empty</p> }
       {}
       <a href="#" className="text-accent-300 fw-200">
-        <button className="cart__checkout-btn bg-orange text-accent-100">
+        <button className="cart__checkout-btn bg-orange text-accent-100 capitalize">
           checkout
         </button>
       </a>
@@ -83,7 +87,7 @@ const UserInfoSection = () => {
       style={{ "--gap": "2rem" }}
     >
       <div className="user__cart pos-rel" style={{ width: "1.5rem" }}>
-        <button onClick={toggleShowCart} style={{'cursor': 'pointer'}}>
+        <button onClick={toggleShowCart} style={{'cursor': 'pointer', 'backgroundColor': 'transparent'}}>
           <img src={Cart} className="w-100" />
         </button>
         <span className="user__cart-count pos-abs text-accent-100 bg-orange">
